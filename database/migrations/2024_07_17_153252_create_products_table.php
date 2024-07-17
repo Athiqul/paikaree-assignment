7b170704dfd973410e65887c4f3f45a726797bf8
Athiqulhasan <athiqulhasan.4@gmail.com>
2024-07-17 21:40:26 +0600

    Products Table DB Schema Done

---
+++ database/migrations/2024_07_17_153252_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('discount')->default(0);
+            $table->string('thumbnail');
+            $table->enum('status',['1','0'])->default('1');//1 for published and 0 for unpublished
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
